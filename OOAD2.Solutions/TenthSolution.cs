770ac7cd59c59edfc31d8d1011a0b1032b7a954d
LAPTOP\work <ichiru98@mail.ru>
2025-09-27 00:08:04 +0300

    Ninth Solution Reflection and Tenth Solution

---
+++ OOAD2.Solutions/TenthSolution.cs
@@ -0,0 +1,18 @@
+﻿namespace OOAD2.Solutions;
+
+public class Vehicle
+{ 
+    public virtual void Start()
+    {
+        Console.WriteLine("Start Vehicle");
+    }
+}
+
+public class SportCar : Vehicle
+{
+    // Ключевое слово sealed запрещает переопределение виртуального метода в наследнике.
+    public sealed override void Start()
+    {
+        Console.WriteLine("Start Sport Car");
+    }
+}
