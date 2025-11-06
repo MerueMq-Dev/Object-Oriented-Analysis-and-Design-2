2854dad64150b4b2cb4876ac21019aca26c9e8f0
PC <icihru98@mai.ru>
2025-11-07 00:37:38 +0300

    Eighteenth Solution Reflection and Nineteenth Solution

---
+++ OOAD2.Solutions/EighteenthSolutionReflection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAD2.Solutions
+{
+    public class EighteenthSolutionReflection
+    {
+        // Моё решение технически корректно и демонстрирует понимание отношения IS-A, но отличается от эталонного по подходу.
+        // Я использовал примеры из предметной области программирования (электронный чек, банковские счета), в то время как
+        // эталонное решение опиралось на простые, общепонятные примеры из реального мира (томат-овощ, заяц-животное).
+    }
+}
